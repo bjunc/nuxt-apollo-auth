8e5925db38e7f9d712f4f727a5e4f3bd55ea0dc2
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-08-28 20:23:39 -0400

    tabs for life

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  rules: {},
-  globals: {}
+	root: true,
+	parser: 'babel-eslint',
+	env: {
+		browser: true,
+		node: true
+	},
+  	extends: 'standard',
+ 	// required to lint *.vue files
+	plugins: ['html'],
+	// add your custom rules here
+	rules: {
+		// tabbed indentation
+		'indent': ['error', 'tab'],
+		'no-tabs': 0
+	},
+	globals: {}
 }
