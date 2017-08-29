eb80f05d910b7b257141ab6852e7f68214f4bc0d
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-08-29 09:12:17 -0400

    removing session logic.  Better to have separate

--- store/index.js
+++ store/index.js
@@ -1,15 +1,11 @@
 import Vuex 	from 'vuex'
-import cookie 	from 'cookie'
 
 const createStore = () => {
 	return new Vuex.Store({
 		state: { token: null },
 		actions: {
 			nuxtServerInit ({ state, commit }, { req }) {
-				if (req.headers['cookie']) {
-					let cookies = cookie.parse(req.headers['cookie'])
-					if (cookies.UJSSESSID) state.token = cookies.UJSSESSID
-				}
+				state.token = 'ABC123'
 			}
 		}
 	})
