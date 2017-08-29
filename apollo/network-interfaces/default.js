84316406c86faed30e142b572f76b5911f323aec
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-08-28 20:24:01 -0400

    converted to tabs.  tried spaces, sorry, it’s horrible…

--- apollo/network-interfaces/default.js
+++ apollo/network-interfaces/default.js
@@ -1,7 +1,6 @@
-
 import { createNetworkInterface } from 'apollo-client'
 
 export default createNetworkInterface({
-  uri: 'https://edgewiserealty.com/graphql',
-  transportBatching: true
+	uri: 'https://edgewiserealty.com/graphql',
+	transportBatching: true
 })
